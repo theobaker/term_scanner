65b34b357f026cd815bc0cb30db80a5d83290c4a
Theo Baker <tabsterbaker@gmail.com>
2020-10-29 22:25:18 -0400

    determined how to output results (method not application, yet)

--- src/AdFinder.java
+++ src/AdFinder.java
@@ -1,4 +1,22 @@
-
+import java.io.*;
+import java.util.*;
 public class AdFinder {
+	public ArrayList<String> findAdAgreements(String nameOfFile){
 
+		String filename = nameOfFile;
+		BufferedReader reader;
+		//read through the given file to extract sections beginning with "ad"
+		try {
+			reader = new BufferedReader(new FileReader(filename));
+
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+
+
+
+		return null;
+
+	}
 }
